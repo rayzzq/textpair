0b977956fe7cd3119a10a6913c3b9c8bc43105f1
rayzzq <max.zzq@hotmail.com>
2022-10-05 01:35:30 +0800

    doc_encoder forward is work now

--- model/doc_encoder.py
+++ model/doc_encoder.py
@@ -1,64 +1,126 @@
-from transformers import AutoModel, AutoTokenizer
-import torch.nn as nn
-import torch
 
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+import pickle
 import hydra
 
+from transformers import AutoModel, AutoTokenizer
 
-class SentEncoder(nn.Module):
-    def __init__(self, cfg):
-        super(SentEncoder, self).__init__()
-        self.encoder = AutoModel.from_pretrained(cfg.pretrain_model_name_or_path)
-        self.pooling_type = cfg.pooling_type
 
-    def forward(self, *input, **kwargs):
+class ScaleAttention(nn.Module):
+    def __init__(self, temp, p=0.2):
+        super(ScaleAttention, self).__init__()
+        self.dropout = nn.Dropout(p=p)
+        self.temp = temp
 
-        out = self.encoder(*input, **kwargs, output_hidden_states=True)
+    def forward(self, q, k, v, mask):
+        # b, seq, n, dv
+        atten = torch.matmul(q / self.temp, k.transpose(-1, -2))
+        if mask is not None:
+            atten.masked_fill(mask, 1e-9)
 
-        if self.pooling_type == 'cls':
-            return out.last_hidden_state[:, 0]  # [batch, 768]
+        print("-"*10 + "debug" + "-"*10)
+        print(q.shape)
+        print(atten.shape)
+        print(mask.shape)
+        print("-"*10 + "debug" + "-"*10)
 
-        if self.pooling_type == 'pooler':
-            return out.pooler_output            # [batch, 768]
+        atten = F.softmax(atten, dim=-1)
+        atten = self.dropout(atten)
+        out = torch.matmul(atten, v)
+        return out, atten
 
-        if self.pooling_type == 'last-avg':
-            last = out.last_hidden_state.transpose(1, 2)    # [batch, 768, seqlen]
-            return torch.avg_pool1d(last, kernel_size=last.shape[-1]).squeeze(-1)
+
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, n_head, d_model, d_k=1536, d_v=1536, dropout=0.2):
+        super(MultiHeadAttention, self).__init__()
+        self.n_head = n_head
+        self.d_k = d_k
+        self.d_v = d_v
+        self.d_model = d_model
+
+        self.w_qs = nn.Linear(d_model, n_head * d_k)
+        self.w_ks = nn.Linear(d_model, n_head * d_k)
+        self.w_vs = nn.Linear(d_model, n_head * d_v)
+
+        self.attention = ScaleAttention(temp=d_k ** 0.5)
+
+        self.fc = nn.Linear(n_head * d_v, d_model, bias=False)
+
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = nn.LayerNorm(d_model)
+
+    def forward(self, q, k, v, mask=None):
+        # q k v: [b, seq, d_model]
+        # mask: [b, seq]
+
+        d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
+        sz_b, len_q, _ = q.size()
+        sz_b, len_k, _ = k.size()
+        sz_b, len_v, _ = v.size()
+
+        residual = q
+
+        q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
+        k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
+        v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
+
+        q = q.transpose(1, 2).contiguous().view(-1, len_q, d_k)
+        k = k.transpose(1, 2).contiguous().view(-1, len_k, d_k)
+        v = v.transpose(1, 2).contiguous().view(-1, len_v, d_v)
+
+        if mask is not None:
+            mask = mask.view(sz_b, 1, len_q, 1)
+        output, attention = self.attention(q, k, v, mask=mask)
+
+        output = output.view(sz_b, n_head, len_q, d_v)
+        output = output.transpose(1, 2).contiguous().view(sz_b, len_q, -1)
+
+        output = self.dropout(self.fc(output))
+        output = self.layer_norm(output + residual)
+
+        return output, attention
 
 
 class DocAttentionBlock(nn.Module):
     def __init__(self, cfg):
         super().__init__()
-        self.attention = nn.MultiheadAttention(embed_dim=cfg.hidden_size,
-                                               num_heads=cfg.num_heads,
-                                               dropout=cfg.dropout,
-                                               bias=False,
-                                               batch_first=True)
+
+        self.attention = MultiHeadAttention(d_model=cfg.hidden_size,
+                                            n_head=cfg.num_heads,
+                                            dropout=cfg.dropout)
 
         self.ln = nn.LayerNorm(cfg.hidden_size)
         self.dropout = nn.Dropout(cfg.dropout)
+        self.activation = nn.GELU()
+        self.fc = nn.Linear(cfg.hidden_size, cfg.hidden_size)
 
-    def forward(self, x, mask):
+    def forward(self, x, mask=None):
         residual = x
-        x = self.attention(x, x, x, key_padding_mask=mask)[0]
+        x, _ = self.attention(x, x, x, mask=mask)
         x = self.dropout(x)
-        x += residual
-        x = self.ln(x)
+        x = self.fc(x)
+        x = self.activation(x)
+        x = self.ln(x + residual)
         return x
 
 
 class DocTransformer(nn.Module):
     def __init__(self, cfg):
         super().__init__()
-        self.activation = nn.GELU()
+        self.fc = nn.Linear(cfg.hidden_size * 2, cfg.hidden_size)
+        self.ln = nn.LayerNorm(cfg.hidden_size)
         self.blocks = nn.ModuleList([DocAttentionBlock(cfg) for _ in range(cfg.num_layers)])
 
-    def forward(self, x, mask):
+    def forward(self, x, mask=None):
+        x = self.fc(x)
         residual = x
         for block in self.blocks:
             x = block(x, mask)
-            x = self.activation(x)
-        x += residual
+        x = self.ln(x + residual)
         return x
 
 
@@ -66,78 +128,75 @@ class DocEncoder(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.cfg = cfg
-        self.sent_encoder = SentEncoder(cfg.sent_encoder)
         self.doc_encoder = DocTransformer(cfg)
+        self.classifier = nn.Sequential(nn.Dropout(cfg.dropout),
+                                        nn.Linear(cfg.hidden_size, cfg.hidden_size // 2),
+                                        nn.GELU(),
+                                        nn.Linear(cfg.hidden_size // 2, cfg.num_classes),)
 
-        self.doc_cls_embedding = nn.Parameter(torch.randn(cfg.hidden_size))
-        self.doc_pad_embedding = nn.Parameter(torch.randn(cfg.hidden_size))
+        self.doc_cls_embedding = nn.Parameter(torch.randn(cfg.hidden_size * 2))
+        self.doc_pad_embedding = nn.Parameter(torch.randn(cfg.hidden_size * 2))
 
-    def forward(self, docs):
-        # batch_size, max_doc_seq_len, hidden_size
-        doc_seqs, doc_mask = self._get_doc_embeds(docs)
-        doc_seqs = self.doc_encoder(doc_seqs, doc_mask)
-        return doc_seqs, doc_mask
+    def forward(self, doc_seqs, doc_scores):
+        doc_seqs, doc_scores = self._pad_to_longest(doc_seqs, doc_scores)
+        doc_seqs = self.doc_encoder(doc_seqs, doc_scores)
+        cls_emb = doc_seqs[:, 0, ...]
+        logits = self.classifier(cls_emb)
+        return doc_seqs, logits
 
-    def _add_cls_pad(self, tensor, max_seq_len):
-        cls_emb = self.doc_cls_embedding
-        pad_emb = self.doc_pad_embedding
+    def _pad_cls_seqs(self, tensor, max_seq_len):
+        cls_emb = self.doc_cls_embedding.unsqueeze(0)
+        pad_emb = self.doc_pad_embedding.unsqueeze(0)
         padded_seq = [cls_emb]
         padded_seq.append(tensor)
         padded_seq.extend([pad_emb] * (max_seq_len - tensor.shape[0]))
-        padded_seq = torch.vstack(padded_seq)
+        padded_seq = torch.cat(padded_seq, dim=0)
         return padded_seq
 
-    def _get_mask(self, doc_lens):
-        max_seq_len = max(doc_lens)
-        mask = torch.ones(len(doc_lens), max_seq_len + 1)
-        for i, l in enumerate(doc_lens):
-            mask[i, :l + 1] = 0
-        return mask.bool()
+    def _pad_cls_socres(self, doc_score, max_seq_len):
+        scores = [torch.ones(1)]
+        scores.append(doc_score)
+        scores.append(torch.zeros(max_seq_len - doc_score.shape[0]))
+        padded_scores = torch.cat(scores, dim=0)
+        return padded_scores
 
-    def _pad_doc_seq_longest(self, doc_seqs, doc_lens):
+    def _pad_to_longest(self, doc_seqs, doc_scores):
+        doc_lens = [len(seq) for seq in doc_seqs]
         max_seq_len = max(doc_lens)
         for i in range(len(doc_seqs)):
-            padded_seq = self._add_cls_pad(doc_seqs[i], max_seq_len)
+            padded_seq = self._pad_cls_seqs(doc_seqs[i], max_seq_len)
+            padded_socres = self._pad_cls_socres(doc_scores[i], max_seq_len)
+            doc_scores[i] = padded_socres.unsqueeze(0)
             doc_seqs[i] = padded_seq.unsqueeze(0)
-        return torch.vstack(doc_seqs)
 
-    def _get_doc_embeds(self, docs):
-        doc_seqs = []
-        doc_lens = []
-        for doc in docs:
-            sents_embed = self.sent_encoder(**doc)
-            doc_lens.append(sents_embed.shape[0])
-            doc_seqs.append(sents_embed)
+        doc_seqs = torch.vstack(doc_seqs)
+        doc_socres = torch.vstack(doc_scores)
 
-        doc_seqs = self._pad_doc_seq_longest(doc_seqs, doc_lens)
-        doc_mask = self._get_mask(doc_lens)
-        return doc_seqs, doc_mask
+        return doc_seqs, doc_socres
+
+
+def load_data(file_path):
+    with open(file_path, 'rb') as f:
+        data = pickle.load(f)
+
+    emb_ab = []
+    rel_ab = []
+    for sample in data[0:5]:
+        emb_ab.append(torch.tensor(sample['emb_ab']))
+        rel_ab.append(torch.tensor(sample['rel_ab']))
+
+    return emb_ab, rel_ab
 
 
 @hydra.main(config_path='./', config_name='config', version_base=None)
 def test_forward(cfg):
+    cfg = cfg.doc_encoder
+    model = DocEncoder(cfg)
+    emb_ab, rel_ab = load_data(cfg.data_path)
+    doc_seqs, logits = model(emb_ab, rel_ab)
 
-    def encode_doc(sents, tokenizer):
-        encoded = tokenizer(sents, padding="longest", truncation=True, return_tensors='pt', max_length=512)
-        return encoded
-
-    model = DocEncoder(cfg.doc_encoder)
-    doc = [r"公诉机关指控,2018年8月底,被告人XXX付给被告人朱德印人民币1000元费用,后XXX印制作了XXX的伪造身份证,并安排学生XXX另案处理于2018年10月27日、28日在城阳五中考点代替XXX参加2018年成人高等学校招生全国统一考试,后被查获。",
-           r"被告人XXX于2018年11月9日主动至投案;被告人XXX于2018年10月29日主动至投案。",
-           r"  经审理查明的事实与公诉机关的指控一致。",
-           r"另查明,根据黑龙江省哈尔滨市道外区司法局调查评估,被告人XXX适用社区矫正,该已缴纳罚金人民币五千元;根据山东省青岛市黄岛区司法局调查评估,被告人XXX适用社区矫正,该已缴纳罚金人民币五千元。",
-           r"经审理查明的事实与公诉机关的指控一致。",
-           r"另查明,根据黑龙江省哈尔滨市道外区司法局调查评估,被告人XXX适用社区矫正,该已缴纳罚金人民币五千元;根据山东省青岛市黄岛区司法局调查评估,被告人XXX适用社区矫正,该已缴纳罚金人民币五千元。",
-           r"本院认为,被告人XXX让他人代替自己参加法律规定的国家考试,其行为构成代替考试罪,依法应予惩处;被告人XXX印在法律规定的国家考试中,策划、安排他人进行作弊,其行为构成组织考试作弊罪,依法应予惩处。",
-           r"公诉机关指控被告人犯罪的事实清楚,证据确实、充分,罪名成立,量刑建议适当,本院予以采纳。",
-           r"二被告人系自首,且自愿认罪认罚,依法可从轻处罚;二人均主动缴纳罚金,可酌情从轻处罚。",
-           r"辩护人所提与上述情节相关的辩护意见,本院予以采纳,其他辩护意见酌情予以考虑"]
-
-    tokenizer = AutoTokenizer.from_pretrained("cyclone/simcse-chinese-roberta-wwm-ext")
-    docs = [encode_doc(doc[:i + 2], tokenizer) for i in range(4)]
-    doc_seqs, doc_mask = model(docs)
     print(doc_seqs.shape)
-    print(doc_mask.shape)
+    print(logits.shape)
 
 
 if __name__ == "__main__":
