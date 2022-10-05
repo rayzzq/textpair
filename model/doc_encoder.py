2648279c61e37bd7eafd1724f0860bf98520a971
rayzzq <max.zzq@hotmail.com>
2022-10-05 13:11:31 +0800

    add layer decay attention add Collate add tag_loss

--- model/doc_encoder.py
+++ model/doc_encoder.py
@@ -13,14 +13,16 @@ import hydra
 import pytorch_lightning as pl
 import torchmetrics
 from pytorch_lightning.loggers import TensorBoardLogger
-
+import math 
 
 class ScaleAttention(nn.Module):
-    def __init__(self, temp, p=0.2):
+    def __init__(self, temp, dropout=0.2, depth=1):
         super(ScaleAttention, self).__init__()
-        self.dropout = nn.Dropout(p=p)
+        self.dropout = nn.Dropout(p=dropout)
         self.temp = temp
-
+        self.depth = depth
+        self.r = math.exp(4 - 2 * self.depth) / (math.exp(4 - 2 * self.depth) + 1)
+        
     def forward(self, q, k, v, mask):
         # b, seq, n, dv
         atten = torch.matmul(q / self.temp, k.transpose(-1, -2))
@@ -28,7 +30,9 @@ class ScaleAttention(nn.Module):
         # TODO: is this correct to use mask label
         if mask is not None:
             atten.masked_fill_(mask.bool(), 1e-9)
-
+        
+        new_mask = mask.bool().float()
+        new_mask = (1 - self.r) * new_mask + self.r * mask 
         # print("-"*10 + "debug" + "-"*10)
         # print(q.shape)
         # print(atten.shape)
@@ -36,7 +40,7 @@ class ScaleAttention(nn.Module):
         # print("-"*10 + "debug" + "-"*10)
 
         atten = F.softmax(atten, dim=-1)
-        atten = atten * mask
+        atten = atten * new_mask
         atten = self.dropout(atten)
         out = torch.matmul(atten, v)
 
@@ -45,7 +49,7 @@ class ScaleAttention(nn.Module):
 
 class MultiHeadAttention(nn.Module):
 
-    def __init__(self, n_head, d_model, d_k=1536, d_v=1536, dropout=0.2):
+    def __init__(self, n_head, d_model, d_k=1536, d_v=1536, dropout=0.2, depth=1):
         super(MultiHeadAttention, self).__init__()
         self.n_head = n_head
         self.d_k = d_k
@@ -56,7 +60,7 @@ class MultiHeadAttention(nn.Module):
         self.w_ks = nn.Linear(d_model, n_head * d_k)
         self.w_vs = nn.Linear(d_model, n_head * d_v)
 
-        self.attention = ScaleAttention(temp=d_k ** 0.5)
+        self.attention = ScaleAttention(temp=d_k ** 0.5, dropout=dropout, depth=depth)
 
         self.fc = nn.Linear(n_head * d_v, d_model, bias=False)
 
@@ -97,12 +101,13 @@ class MultiHeadAttention(nn.Module):
 
 
 class DocAttentionBlock(nn.Module):
-    def __init__(self, cfg):
+    def __init__(self, cfg, depth=1):
         super().__init__()
 
         self.attention = MultiHeadAttention(d_model=cfg.hidden_size,
                                             n_head=cfg.num_heads,
-                                            dropout=cfg.dropout)
+                                            dropout=cfg.dropout,
+                                            depth = depth)
 
         self.ln = nn.LayerNorm(cfg.hidden_size)
         self.dropout = nn.Dropout(cfg.dropout)
@@ -124,7 +129,7 @@ class DocTransformer(nn.Module):
         super().__init__()
         self.fc = nn.Linear(cfg.hidden_size * 2, cfg.hidden_size)
         self.ln = nn.LayerNorm(cfg.hidden_size)
-        self.blocks = nn.ModuleList([DocAttentionBlock(cfg) for _ in range(cfg.num_layers)])
+        self.blocks = nn.ModuleList([DocAttentionBlock(cfg, depth=i) for i in range(cfg.num_layers)])
 
     def forward(self, x, mask=None):
         x = self.fc(x)
@@ -148,45 +153,12 @@ class DocEncoder(nn.Module):
         self.doc_pad_embedding = nn.Parameter(torch.randn(cfg.hidden_size * 2))
 
     def forward(self, doc_seqs, doc_scores):
-        doc_seqs, doc_scores = self._pad_to_longest(doc_seqs, doc_scores)
         doc_seqs = self.doc_encoder(doc_seqs, doc_scores)
         doc_seqs = doc_seqs.transpose(1, 2)
         cls_emb = torch.avg_pool1d(doc_seqs, kernel_size=doc_seqs.shape[-1]).squeeze(-1)
         logits = self.classifier(cls_emb)
+        doc_seqs = doc_seqs.transpose(1, 2).contiguous()
         return doc_seqs, logits
-
-    def _pad_cls_seqs(self, tensor, max_seq_len):
-        device = self.doc_pad_embedding.device
-        pad_emb = self.doc_pad_embedding.unsqueeze(0)
-        padded_seq = []
-        padded_seq.append(tensor.to(device))
-        padded_seq.extend([pad_emb] * (max_seq_len - tensor.shape[0]))
-        padded_seq = torch.cat(padded_seq, dim=0)
-        return padded_seq
-
-    def _pad_cls_socres(self, doc_score, max_seq_len):
-        device = self.doc_pad_embedding.device
-        scores = []
-        scores.append(doc_score.to(device))
-        scores.append(torch.zeros(max_seq_len - doc_score.shape[0]).to(device))
-        padded_scores = torch.cat(scores, dim=0)
-        return padded_scores
-
-    def _pad_to_longest(self, doc_seqs, doc_scores):
-        
-        doc_lens = [len(seq) for seq in doc_seqs]
-        max_seq_len = max(doc_lens)
-        
-        for i in range(len(doc_seqs)):
-            padded_seq = self._pad_cls_seqs(doc_seqs[i], max_seq_len)
-            padded_socres = self._pad_cls_socres(doc_scores[i], max_seq_len)
-            doc_scores[i] = padded_socres.unsqueeze(0)
-            doc_seqs[i] = padded_seq.unsqueeze(0)
-            
-        doc_seqs = torch.vstack(doc_seqs)
-        doc_socres = torch.vstack(doc_scores)
-
-        return doc_seqs, doc_socres
 
 
 class DocModel(pl.LightningModule):
@@ -197,26 +169,50 @@ class DocModel(pl.LightningModule):
         self.hparams.update(cfg.train_args)
         self.save_hyperparameters()
         self.model = DocEncoder(cfg)
+        self.tag_classifier = nn.Sequential(nn.Dropout(cfg.dropout),
+                                            nn.Linear(cfg.hidden_size, cfg.hidden_size // 2),
+                                            nn.GELU(),
+                                            nn.Linear(cfg.hidden_size // 2, 2),)
         self.train_acc = torchmetrics.Accuracy()
         self.valid_acc = torchmetrics.Accuracy()
 
-    def forward(self, x):
-        doc_seqs, logits = self.model(x)
+        
+        
+    def forward(self, doc_seqs, doc_scores):
+        doc_seqs, logits = self.model(doc_seqs, doc_scores)
         return doc_seqs, logits
 
+
+    def _tag_loss(self, seqs, tags, mask):
+        # seq b * s * h
+        # tag b * s 
+        # mask b * s
+        logit = self.tag_classifier(seqs).view(-1, 2)
+        tags = tags.view(-1).long()
+        mask = mask.bool().view(-1)
+        loss = F.cross_entropy(logit[mask], tags[mask], reduction='mean')
+        return loss
+        
+        
     def training_step(self, batch, batch_idx):
-        input = batch.get("input")
+        emb_ab = batch.get("emb_ab")
+        logits_ab = batch.get("logits_ab")
         label = batch.get("label")
-        doc_seqs, logits = self.model(*input)
-        loss = F.cross_entropy(logits, label)
+        rel_ab = batch.get("rel_ab")
+        doc_seqs, logits = self(emb_ab, logits_ab)
+        doc_loss = F.cross_entropy(logits, label)
+        tag_loss = self._tag_loss(doc_seqs, rel_ab, logits_ab)
+        loss = 0.8 * doc_loss + 0.2 * tag_loss
         self.log("train_loss", loss, on_step=True)
         self.train_acc.update(logits, label)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        input = batch.get("input")
+        emb_ab = batch.get("emb_ab")
+        logits_ab = batch.get("logits_ab")
         label = batch.get("label")
-        doc_seqs, logits = self.model(*input)
+        
+        doc_seqs, logits = self.model(emb_ab, logits_ab)
         self.valid_acc.update(logits, label)
 
     def training_epoch_end(self, outputs):
@@ -243,14 +239,14 @@ class DocModel(pl.LightningModule):
         return [optimizer], [lr_scheduler]
 
     def train_dataloader(self):
-        return self.train_data
+        return self.train_dl
 
     def val_dataloader(self):
-        return self.valid_data
+        return self.val_dl
 
     def load_dataloader(self, cfg):
-        collate_fn = self.collate_fn
-
+        collate_fn = Collate.from_model(self.model)
+        
         train_data = pickle.load(open(cfg.train_data_path, "rb"))
         valid_data = pickle.load(open(cfg.valid_data_path, "rb"))
 
@@ -266,25 +262,79 @@ class DocModel(pl.LightningModule):
                             collate_fn=collate_fn,
                             num_workers=4)
 
-        self.train_data = train_dl
-        self.valid_data = val_dl
+        self.train_dl = train_dl
+        self.val_dl = val_dl
 
-    @staticmethod
-    def collate_fn(batch):
+
+
+class Collate(object):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model 
+        self.device = self.model.doc_pad_embedding.device
+        
+    def __call__(self, batch):
         label = []
         emb_ab = []
         logits_ab = []
+        rel_ab = []
         for sample in batch:
             emb_ab.append(torch.tensor(sample.get("emb_ab")))
-            
+
             logits = torch.tensor(sample.get("logits_ab"))
             logits = torch.softmax(logits, dim=-1)
             logits = logits[:, 1]
             logits_ab.append(logits)
-            
             label.append(sample.get("label"))
-
-        return {"input": (emb_ab, logits_ab), "label": torch.tensor(label)}
+            
+            rel_ab.append(torch.tensor(sample.get("rel_ab")))
+        
+        emb_ab, logits_ab, rel_ab = self._pad_to_longest(emb_ab, logits_ab, rel_ab)
+        
+        return {"emb_ab": emb_ab, "logits_ab": logits_ab, "label": torch.tensor(label), "rel_ab": rel_ab}
+    
+    
+    def _pad_emb_seqs(self, tensor, max_seq_len):
+        device = self.device
+        pad_emb = self.model.doc_pad_embedding.unsqueeze(0)
+        padded_seq = []
+        padded_seq.append(tensor.to(device))
+        padded_seq.extend([pad_emb] * (max_seq_len - tensor.shape[0]))
+        padded_seq = torch.cat(padded_seq, dim=0)
+        return padded_seq
+    
+    def _pad_tag_seqs(self, tag, max_seq_len):
+        device = self.device
+        padded_tag = []
+        padded_tag.append(tag.to(device))
+        padded_tag.append(torch.zeros(max_seq_len - tag.shape[0]).to(device))
+        padded_tag = torch.cat(padded_tag, dim=0)
+        return padded_tag
+    
+    
+    def _pad_to_longest(self, doc_seqs, doc_scores, doc_tags):
+        doc_lens = [len(seq) for seq in doc_seqs]
+        max_seq_len = max(doc_lens)
+        
+        for i in range(len(doc_seqs)):
+            padded_seq = self._pad_emb_seqs(doc_seqs[i], max_seq_len)
+            padded_scores = self._pad_tag_seqs(doc_scores[i], max_seq_len)
+            padded_tag = self._pad_tag_seqs(doc_tags[i], max_seq_len)
+            
+            doc_scores[i] = padded_scores.unsqueeze(0)
+            doc_seqs[i] = padded_seq.unsqueeze(0)
+            doc_tags[i] = padded_tag.unsqueeze(0)
+            
+            
+        doc_seqs = torch.vstack(doc_seqs)
+        doc_socres = torch.vstack(doc_scores)
+        doc_tags = torch.vstack(doc_tags)
+        
+        return doc_seqs, doc_socres, doc_tags
+    
+    @classmethod
+    def from_model(cls, model):
+        return cls(model)
 
 
 @hydra.main(config_path="./", config_name="config", version_base="1.2")
